21b55d119455975f6481fb4340459144264310a9
Dongmin_Kim <66549954+dongmin30@users.noreply.github.com>
2023-05-05 07:46:17 +0000

    add tuple py

---
+++ code/tuple-basic.py
@@ -0,0 +1,34 @@
+# %%
+p = 'python'
+y = 'python'
+print(p, y)
+# %%
+# immutable: 문자열, 정수, 실수
+p[0] = 'j'
+# %%
+print(p is y)
+# %%
+print(id(p), id(y))
+# %%
+t = ()
+t = tuple()
+type(t)
+# %%
+n = 1
+type(n)
+# %%
+m = (1, )
+type(m)
+# %%
+t1 = (1, 2, 3)
+t2 = (1, 2, 3)
+print(t1 == t2)
+print(id(t1), id(t2))
+print(id(t1) == id(t2))
+print(t1 is t2)
+# %% mutable(수정가능) : 리스트, 딕셔너리
+# t1[0] = 10 튜플은 수정할 수 없다.
+lst = [1, 2, 3]
+lst[1] = 20
+lst
+# %%
